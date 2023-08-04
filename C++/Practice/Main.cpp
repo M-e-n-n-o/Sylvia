3ceb971719f1b775f14483c9a5f98f1a4138a2cf
Menno <mennobil02@gmail.com>
2023-08-04 10:36:48 +0200

    Back to basics geoefend

--- C++/Practice/Main.cpp
+++ C++/Practice/Main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
+using namespace std;
+
+bool Varkentje(int a, int b)
+{
+	if (a == 30 || b == 30)
+	{
+		return true;
+	}
+	else if (a + b == 30)
+	{
+		return true;
+	}
+
+	return false;
+}
+
 int main()
 {
+	int Menno = 15;
+	int Sylvia = 20;
+
+	bool Uitkomst = Varkentje(Menno, Sylvia);
+
+	cout << Uitkomst << endl;
+
 	return 0;
 }
